2ac0ae980307dbdcbc7c0af9236718f2cf194713
Sripadhavignesh3108 <sripadhavignesh@gmail.com>
2024-06-27 21:40:00 +0530

    time and date Method

---
+++ datemethod.js
@@ -0,0 +1,61 @@
+const prompt = require("prompt-sync")();
+
+// // // // FINDING THE DATE AND TIME IN YYYY-MM-DD HH-MM-SS FORMAT
+// // // // -------------------------------------------------------
+// function currenttime(){
+//     const a=new Date();
+// console.log(`Current Date and Time is : ${a.getFullYear()}-${a.getMonth()}-${a.getDate()}  ${a.getHours()}:${a.getMinutes()}:${a.getSeconds()}`)
+// }
+// currenttime()
+
+
+
+
+
+// // // // FINDING AGE USING DATE METHOD
+// // // // -----------------------------
+// function age(current){
+//     const today=new Date()
+//     var birthday=new Date(current)
+//     var age=today.getFullYear() - birthday.getFullYear()
+//        if(today.getMonth()<birthday.getMonth() || today.getMonth()==birthday.getMonth() &&
+//        today.getDate() < birthday.getDate()){
+//      age--;
+//   }
+//     return age;
+// }
+// console.log(`Your age is :${age(prompt("Enter Your year of birth :"))}`)
+
+
+
+// function Time(dob){
+//     var birthday=new Date(dob)
+//     let today=new Date()
+//     var days=birthday.getDate()-today.getDate()
+//     return days;
+// }
+// console.log(`Days until Next birthday is : ${Time(prompt("Enter Your Date of birth in YYYY-MM-DD Format :"))}`)
+
+
+
+
+// function leap(year){
+//     if(year%4==0){
+//         return("its a leap year")
+//     }
+//     else return("not a leap year")
+// }
+// console.log(leap(prompt("enter a year : ")))
+
+
+
+
+function days(){
+let date=new Date("")
+let tie=date.getTime();
+let mm=
+return mm
+}
+
+console.log(days())
+
